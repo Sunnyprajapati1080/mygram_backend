afb38a66078c8920439c173d8a997cb9075f08c0
sunny_prajapati_23 <saurabh1080i@gmail.com>
2026-05-27 07:53:30 +0530

    Add CORS support and improve server initialization

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+
 let app = express();
 const port = 8080;
-app.listen(port);
 
-module.exports = {app,express}
+app.use(cors({
+    origin: 'https://mygram-phi.vercel.app'
+}));
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = { app, express };
